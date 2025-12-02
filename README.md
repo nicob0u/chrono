cdf11eae673429220db3259a5e8d25e11ca8064f
Nico <nicob0unico@gmail.com>
2025-12-02 14:05:01 +0330

    build

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 ### chrono
 
 a tiny command-line work period tracker.<br>
-add the program's folder to your system path and you're good to go.
 
 ### usage
 
--- main.lua
+++ main.lua
@@ -1,19 +1,9 @@
 local exe_path = arg[0]:gsub("[^/\\]*$", "")
 package.path = exe_path .. "?.lua;" .. exe_path .. "?/init.lua;" .. package.path
 
--- get script dir
-local info = debug.getinfo(1, "S")
-local script_dir
-
-if info.source:sub(1, 1) == "@" then
-  script_dir = info.source:match("@(.*[/\\])")
-else
-  script_dir = ""
-end
-
-
-package.path = script_dir .. "?.lua;" .. package.path
-
+package.path = exe_path .. "modules/?.lua;" ..
+               exe_path .. "modules/?/init.lua;" ..
+               package.path
 local Period = require("period")
 
 local command = arg[1]
